2e1b5048bdb738ce2ff3ef9ec26f081f86e9f7cd
PetrovDG <dimitar@sitost.com>
2020-08-19 11:21:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.serviceexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton, stopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/serviceexample/MyService.java
@@ -0,0 +1,44 @@
+package com.example.serviceexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    // Извиква се, когато сървисът се създава
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setLooping(true);
+        Toast.makeText(this, "Service started ...", Toast.LENGTH_LONG).show();
+    }
+
+    // Извиква се, когато клиентът се свързва с bindService()
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    // Извиква се, когато сървисът не се изплзва повече и се разрушава
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        Toast.makeText(this, "Service destroyed ...", Toast.LENGTH_LONG).show();
+    }
+}
